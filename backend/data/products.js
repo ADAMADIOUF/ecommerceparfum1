8bfd9493274e58c45fae4aac9cdb70c3b3bb14b3
adama diouf <adamadiouf2017@gmail.com>
2023-06-14 12:00:42 -0500

    done

---
+++ backend/data/products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    
+    name: 'Product 1',
+    image: '/images/1.jpg',
+    description: 'This is the description of Product 1.',
+    brand: 'Brand A',
+    category: 'Category X',
+    price: 19.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 20,
+  },
+  {
+   
+    name: 'Product 2',
+    image: '/images/2.jpg',
+    description: 'This is the description of Product 2.',
+    brand: 'Brand B',
+    category: 'Category Y',
+    price: 29.99,
+    countInStock: 5,
+    rating: 3.8,
+    numReviews: 10,
+  },
+  {
+   
+    name: 'Product 3',
+    image: '/images/3.jpg',
+    description: 'This is the description of Product 3.',
+    brand: 'Brand C',
+    category: 'Category X',
+    price: 14.99,
+    countInStock: 2,
+    rating: 4.2,
+    numReviews: 8,
+  },
+  {
+   
+    name: 'Product 4',
+    image: '/images/4.jpg',
+    description: 'This is the description of Product 4.',
+    brand: 'Brand A',
+    category: 'Category Z',
+    price: 39.99,
+    countInStock: 0,
+    rating: 4.8,
+    numReviews: 15,
+  },
+  {
+ 
+    name: 'Product 5',
+    image: '/images/5.jpg',
+    description: 'This is the description of Product 5.',
+    brand: 'Brand B',
+    category: 'Category Y',
+    price: 24.99,
+    countInStock: 7,
+    rating: 4.1,
+    numReviews: 12,
+  },
+  {
+  
+    name: 'Product 6',
+    image: '/images/6.jpg',
+    description: 'This is the description of Product 6.',
+    brand: 'Brand C',
+    category: 'Category Z',
+    price: 49.99,
+    countInStock: 3,
+    rating: 4.6,
+    numReviews: 18,
+  },
+]
+export default products
